6e512d0eb063e42d4c7ce8214f4f5c2148bcc1d8
Muhammad Owais <rahneverd@gmail.com>
2024-08-02 12:58:13 +0300

    router added

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
+import router from './router';
 
+// load environment variables
 dotenv.config();
+
+// initialize app
 const app = express();
 
 // set public
 app.use(express.static('./public'));
+
 // set views
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.render('home-guest');
-});
+// set router
+app.use('/', router);
 
+// start app
 app.listen(process.env.PORT, () => {
   console.log(`server running on http://localhost:${process.env.PORT}`);
 });
