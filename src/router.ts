0207f13662ece2cacb34d92066671007be6b11da
Muhammad Owais <rahneverd@gmail.com>
2024-08-02 13:45:53 +0300

    routes structered module wise, controllers added

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import userRouter from './routes/userRouter';
+import postRouter from './routes/postRouter';
 
 const router = express.Router();
 
 router.get('/', (req: express.Request, res: express.Response) => {
   res.render('home-guest');
 });
+
+// auth related routes
+// router.post('/login')
+
+// user related routes
+router.use('/users', userRouter);
+
+// post related routes
+router.post('/posts', postRouter);
 
 export default router;
