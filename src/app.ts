52b1395d0e6f026f5018a7743cb56aac6b544c4b
Muhammad Owais <rahneverd@gmail.com>
2024-08-02 12:32:51 +0300

    style added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 const app = express();
 
+// set public
+app.use(express.static('./public'));
 // set views
 app.set('views', './views');
 app.set('view engine', 'ejs');
